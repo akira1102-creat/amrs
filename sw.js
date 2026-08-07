68fe5e76dde95ff17f2c3558edbc13b0e5faa636
akira1102-creat <umatthewkennethvCV@post.com>
2026-08-07 10:42:48 +0800

    新增零件列表即時搜尋

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'amrs-v1057';
+const CACHE = 'amrs-v1058';
 const ASSETS = ['./', './index.html', './cloud-api.js', './manifest.json', './sw.js'];
 
 self.addEventListener('message', e => {
